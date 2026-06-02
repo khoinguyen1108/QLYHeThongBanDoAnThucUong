a7bd6651fdbc86ba44ea95ebf1130012b4a2cda4
Pham Vu Khoi Nguyen <vukhoinguyenpham@gmail.com>
2026-06-03 04:24:56 +0700

    Remove AI generated scratch files and logs

--- QuanLyBanHangDoAnThucUong/fix_sqlquery_avatar.py
+++
@@ -1,9 +0,0 @@
-﻿with open('resources/SQLQuery.sql', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('EmailKH NVARCHAR(255) NOT NULL,', 'EmailKH NVARCHAR(255) NOT NULL,\n    Avatar NVARCHAR(255) NULL,')
-
-with open('resources/SQLQuery.sql', 'w', encoding='utf-8') as f:
-    f.write(text)
-
-print("Added Avatar to KhachHang in SQLQuery.sql")
